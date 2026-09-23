a2d04e3a9bf21199eecc98b1969af8f74096a686
Mayu200052 <ayuta1977@icloud.com>
2026-09-23 09:29:15 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,31 @@
-const C='genba-v90',A=['./','./index.html','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='genba-kikai-v92';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.mode==='navigate'){
+    event.respondWith(
+      fetch(event.request).then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(c=>c.put('./index.html',copy));
+        return response;
+      }).catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(cached=>cached || fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(c=>c.put(event.request,copy));
+      return response;
+    }))
+  );
+});
