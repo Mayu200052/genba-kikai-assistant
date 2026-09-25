ea76c65c15af058ad4dc6db8fbeb9b62d597907e
Mayu200052 <ayuta1977@icloud.com>
2026-09-25 20:54:31 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='genba-kikai-v100';
+const CACHE='genba-kikai-v103';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
